c3d2f0e14d555c0ac71fc7b559e10e32d336f7bd
Vilen <vilenmoscow@gmail.com>
2017-12-27 03:39:24 +0300

    Update newcntsrt.cpp

--- newcntsrt.cpp
+++ newcntsrt.cpp
@@ -12,23 +12,20 @@ bool read(int *mas, int len) {
 	}
 	return true;
 }
-void countsort(int *mas,int *bet,  int n) {
+void countsort(int *mas,int *bet,  int n){
 	int cnt[256]; 
-	for (int i=0;i<n;i++){
-	  bet[i]=i;
-	}
+	for (int i=0;i<n;i++) bet[i]=i;
 	for (int i = 0; i < 256; i++)  cnt[i] = 0; 
-	for (int i = 0; i < n; i++) cnt[mas[i]] = cnt[mas[i]] + 1;
-	for (int j = 1; j < 256; j++) {
-		cnt[j] = cnt[j] + cnt[j - 1];
-	}
+	for (int i = 0; i < n; i++) cnt[mas[i]] +=  1;
+	for (int j = 1; j < 256; j++) 	cnt[j] += cnt[j - 1];
 	for (int i = n-1; i > -1; i--){
-	   cnt[mas[i]] = cnt[mas[i]] - 1;
+	   cnt[mas[i]] -= 1;
         bet[cnt[mas[i]]] = mas[i];
 	}
 }
 int main() {
-	int len; string o;
+	int len; 
+	string o;
 	getline(cin, o);
 	istringstream stream(o);
 	if (!(stream >> len || len > 0)) {
@@ -39,11 +36,8 @@ int main() {
 	int *bet = new int[len];
 	read(mas, len);
 	for (int i = 0; i < len; i++) {
-
 		if (mas[i] < 0) {
-
 			cout << "An error has occuried while reading input data.";
-
 			return 0;
 		}
 	}
